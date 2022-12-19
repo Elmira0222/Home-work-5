2ff6252660e60900cc231ff533a31f2e5b330900
Elmira <zhan0222@mail.ru>
2022-12-19 17:32:30 +0600

    add tasks

---
+++ task34/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+
+int[] array = new int[new Random().Next(10, 10)];
+for (int Numb=0; Numb<array.Length; Numb++)
+{
+    array[Numb] = new Random().Next(100, 1000);
+    Console.Write($"{array[Numb]} ");
+}
+int Count=0;
+foreach (var Even in array)
+if (Even%2 == 0)
+    Count++;
+Console.WriteLine();
+Console.Write($"an array filled with random positive three-digit numbers: {Count}");
